58699a4ada60405946a7a878307330d8eee472e3
Kharn27 <32738307+Kharn27@users.noreply.github.com>
2026-08-19 23:07:29 +0200

    ESP32: expose touch overlay C bridge

--- ESP32/include/platform_video_c_bridge.h
+++ ESP32/include/platform_video_c_bridge.h
@@ -2,6 +2,7 @@
 #define DOOMRPG_ESP32_PLATFORM_VIDEO_C_BRIDGE_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,6 +10,19 @@ extern "C" {
 
 void* Esp32PlatformVideo_framebuffer(void);
 size_t Esp32PlatformVideo_framebufferSizeBytes(void);
+
+void Esp32PlatformVideo_debugOverlayClear(void);
+void Esp32PlatformVideo_debugOverlaySetZone(int index,
+                                            int16_t logicalLeft,
+                                            int16_t logicalTop,
+                                            int16_t logicalRight,
+                                            int16_t logicalBottom);
+void Esp32PlatformVideo_debugOverlayRefresh(void);
+void Esp32PlatformVideo_debugOverlayMarkTouch(int16_t physicalX,
+                                              int16_t physicalY,
+                                              uint16_t pressure,
+                                              uint16_t rawX,
+                                              uint16_t rawY);
 
 #ifdef __cplusplus
 }
