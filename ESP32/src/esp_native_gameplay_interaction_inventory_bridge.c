f4ddda1cfaa6cd87c0b4143e786ff4d19592dc7f
Kharn27 <32738307+Kharn27@users.noreply.github.com>
2026-08-28 10:01:16 +0200

    esp32: remove redundant interaction corpus wrap bridge

--- ESP32/src/esp_native_gameplay_interaction_inventory_bridge.c
+++
@@ -1,9 +0,0 @@
-#include "esp_native_gameplay_interaction_inventory.h"
-#include "esp_native_gameplay_pickup.h"
-
-void __real_EspNativeGameplayPickup_logCorpus(void);
-
-void __wrap_EspNativeGameplayPickup_logCorpus(void) {
-    __real_EspNativeGameplayPickup_logCorpus();
-    EspNativeGameplayInteractionInventory_log();
-}
