d3836f5a66776c4ec2f9c44f9430303f06c579af
Kharn27 <32738307+Kharn27@users.noreply.github.com>
2026-09-02 20:11:51 +0200

    ESP32: keep activation ownership outside retaliation view

--- ESP32/include/esp_native_gameplay_monster_retaliation.h
+++ ESP32/include/esp_native_gameplay_monster_retaliation.h
@@ -17,8 +17,6 @@ typedef struct EspNativeGameplayMonsterRetaliationView_s {
     uint32_t renderRollbacks;
     uint32_t lethalDeferred;
     uint32_t dogFamiliarDeferred;
-    uint32_t activationDeferred;
-    uint32_t activationObserved;
     uint16_t lastAttackerSpriteIndex;
     uint8_t active;
     uint8_t reserved;
